505946395dc73960d2cc83213c5e594325a34aed
John Marshall <marshall@hep.phy.cam.ac.uk>
2015-12-02 12:22:42 +0000

    Remove unused header files.

--- mnist/include/histgram.h
+++
@@ -1,8 +0,0 @@
-#define X_HIST 		0
-#define Y_HIST		1
-
-#define BITLUT	"/usr/local/hsfsys/lut/bitcount.lut"
-#define LUTLEN		256
-
-void compute_x_hist();
-void compute_y_hist();
--- mnist/include/shuffle.h
+++
@@ -1,7 +0,0 @@
-#include <indxstrc.h>
-
-#define MAX_INDEX	200000
-
-extern int *shuffle_index();
-extern char **shuffle_string();
-extern int *shuffle_integers();
